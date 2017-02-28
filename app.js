d1de8576c5a9f0579663059f5ffa22b3c4330785
Veronica Mihai <Veronica Mihai>
2017-02-28 17:09:50 +0200

    switched to new words after all limbs have been shown and started the end game functionality by resetting all variables and display an alert

--- app.js
+++ app.js
@@ -3,8 +3,16 @@
  	var listOfWords = [],
  	    currentWord = [],
  	    displayWord = [],
+ 	    wordsPerGame = 5,//switch words per game to 5 instead of 10
+ 	    limbsArray = ['l1','l2','l3','l4','l5','l6','l7'],
+ 	    indexWordToGuess = 0,
  	    count = 1,
- 		apiKey="a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+ 		apiKey="a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+ 	    guessed = document.getElementById("guessed-letters"),
+ 	    message = document.getElementById('message-area'),
+ 	    wordContainer = document.getElementById("word"),
+        guessedString = "",   //to display letters already guessed
+        arrLetters = []; //an array to hold the letters already guessed;
 
  	window.onload = function(e){  //when page finishes loading
  	    getRandomWords().then(function(response){ 
@@ -17,7 +25,7 @@
  	       	  alert("Please come back later!");  //if no response from api
  	       }
  	                  
- 	       console.log("promise end",response);
+ 	     //  console.log("promise end",response);
  		}).catch(function(error){
  			console.log("There was an error in retrieving random words",error);
  		});
@@ -51,7 +59,7 @@
 		        }
 		    };
 
-		    xmlhttp.open("GET", "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=noun&excludePartOfSpeech=noun-plural&minCorpusCount=10000&maxCorpusCount=20000&minDictionaryCount=0&maxDictionaryCount=-1&minLength=5&maxLength=7&limit=10&api_key="+apiKey, true);
+		    xmlhttp.open("GET", "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=noun&excludePartOfSpeech=noun-plural&minCorpusCount=10000&maxCorpusCount=20000&minDictionaryCount=0&maxDictionaryCount=-1&minLength=5&maxLength=7&limit="+wordsPerGame+"&api_key="+apiKey, true);
 		    xmlhttp.send();	
 		});
 
@@ -59,28 +67,25 @@
 	}
 
 	function displayCurrentWord(){
-		var wordContainer = document.getElementById("word");
 		var dashes = Array.apply(null, { length: currentWord.length-2 }) ;
 		var addDashes = dashes.map(function(dash){
            return "_";
 		});
+		 displayWord = []; //resets word to display before adding new word to guess
          displayWord.push(currentWord[0]);
          displayWord = displayWord.concat(addDashes);
          displayWord.push(currentWord[currentWord.length-1]);
 
-		// currentWordText = first + [e,r_,_,] + lastLetter
 		wordContainer.innerHTML = displayWord.join(" ");
 
 	}
 
-    var guessed = document.getElementById("guessed-letters");
-    var guessedString = "";   //to display letters already guessed
-    var arrLetters = []; //an array to hold the letters already guessed
+  
 
 	document.getElementById('letter').onkeyup = function(){
 		 var letter = document.getElementById("letter").value;
          console.log(letter);
-	    var wordContainer = document.getElementById("word");
+	   
 	    var found = false;
         currentWord.forEach(function(character,index){
            if(index !==0 && index !== (currentWord.length-1) && 
@@ -92,27 +97,74 @@
            
 		});
 
- 		if(count <=7 && arrLetters.indexOf(letter) < 0 && !found){//if there are still limbs to display and the letter hasn't already been guessed
+ 		 	
+       document.getElementById('letter').value ="";     //reset input value
+       
+       if(count < 8 && arrLetters.indexOf(letter) < 0 && !found){//if there are still limbs to display and the letter hasn't already been guessed
            	 displayLimb(count);
            	 count++;
-        } 	
-       document.getElementById('letter').value ="";     //reset input value
-       var message = document.getElementById('message-area'); // reset message area
-        wordContainer.innerHTML = displayWord.join(" ");    //update the displayed word
-        if (arrLetters.indexOf(letter) != -1){        //if letter has already been guessed
-            message.innerHTML = "That letter has already been guessed";
-        } else {                                     // if a new letter
-          guessedString = guessedString.concat(letter);
-          guessed.innerHTML = guessedString;      //display the guessed letter
-          arrLetters.push(letter);
-          console.log(arrLetters);
-          message.innerHTML = "";
+        
         }
+        
+        if(count < 8){
+        	 wordContainer.innerHTML = displayWord.join(" ");    //update the displayed word
 
-      
+	        if (arrLetters.indexOf(letter) != -1){        //if letter has already been guessed
+	            message.innerHTML = "That letter has already been guessed";
+	        } else {                                     // if a new letter
+	          guessedString = guessedString.concat(letter);
+	          guessed.innerHTML = guessedString;      //display the guessed letter
+	          arrLetters.push(letter);
+	          console.log(arrLetters);
+	          message.innerHTML = "";
+	        }	
+        }
+        else{// if the user has already missed 7 times 
+        	resetWord();//reset all needed variables, labels
+        }
+     
 	}
 
 	function displayLimb(number){
 		document.getElementById("l"+number).className = "show";
+	}
+	function hideLimbs(){
+		 limbsArray.forEach(function(limb){
+		           document.getElementById(limb).className = "hide";
+	     });
+	}
+	function resetWord(){
+		
+		if(indexWordToGuess < listOfWords.length-1){
+		  // display previous word to guess in full 
+		   displayWord =  listOfWords[indexWordToGuess].split(""); 
+		   wordContainer.innerHTML = displayWord.join(" "); 
+		  	//wait two seconds before moving to the next word in the list
+    	   setTimeout(function(){ 
+	    	   	reinitializeValues();
+				//move to the next word to guess in the list	
+			    indexWordToGuess++;	
+			    currentWord = listOfWords[indexWordToGuess].split("");
+	    	   	displayCurrentWord(); 
+    	   }, 2000);
+		   
+
+		}
+		else{
+			reinitializeValues()
+			alert("Game over !");
+			//TO-DO implement code for display end game dialog for starting a new game again	
+		}
+	}
+	function reinitializeValues(){
+		//hide hangman again
+			    hideLimbs();
+				//resets count of missed letters		
+				count =1;
+				//reset guessed letters display
+				guessedString = "";
+				guessed.innerHTML = guessedString; 
+				message.innerHTML = guessedString;
+				arrLetters = [];
 	}
 })();
